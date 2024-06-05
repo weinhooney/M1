8be94c81ca22f7c5429bf25e720db7de407ed3eb
weinhooney <devweinhooney@gmail.com>
2024-06-06 05:06:18 +0900

    Manager 생성

--- Assets/@Scripts/Managers/Core/PoolManager.cs
+++ Assets/@Scripts/Managers/Core/PoolManager.cs
@@ -1,18 +1,107 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Pool;
 
-public class PoolManager : MonoBehaviour
+internal class Pool
 {
-    // Start is called before the first frame update
-    void Start()
+    private GameObject _prefab;
+    private IObjectPool<GameObject> _pool;
+
+    private Transform _root;
+    private Transform Root
     {
-        
+        get
+        {
+            if(null == _root)
+            {
+                GameObject go = new GameObject { name = $"@{_prefab.name}Pool" };
+                _root = go.transform;
+            }
+
+            return _root;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public Pool(GameObject prefab)
     {
-        
+        _prefab = prefab;
+        _pool = new ObjectPool<GameObject>(OnCreate, OnGet, OnRelease, OnDestroy);
+    }
+
+    public void Push(GameObject go)
+    {
+        if(go.activeSelf)
+        {
+            _pool.Release(go);
+        }
+    }
+
+    public GameObject Pop()
+    {
+        return _pool.Get();
+    }
+
+    #region Funcs
+    private GameObject OnCreate()
+    {
+        GameObject go = GameObject.Instantiate(_prefab);
+        go.transform.SetParent(Root);
+        go.name = _prefab.name;
+
+        return go;
+    }
+
+    private void OnGet(GameObject go)
+    {
+        go.SetActive(true);
+    }
+
+    private void OnRelease(GameObject go)
+    {
+        go.SetActive(false);
+    }
+
+    private void OnDestroy(GameObject go)
+    {
+        GameObject.Destroy(go);
+    }
+    #endregion
+}
+
+public class PoolManager
+{
+    private Dictionary<string, Pool> _pools = new Dictionary<string, Pool>();
+
+    public GameObject Pop(GameObject prefab)
+    {
+        if (false == _pools.ContainsKey(prefab.name))
+        {
+            CreatePool(prefab);
+        }
+
+        return _pools[prefab.name].Pop();
+    }
+
+    public bool Push(GameObject go)
+    {
+        if (false == _pools.ContainsKey(go.name)) { return false; }
+
+        _pools[go.name].Push(go);
+
+        return true;
+    }
+
+    public void Clear()
+    {
+        _pools.Clear();
+    }
+
+    private void CreatePool(GameObject original)
+    {
+        Pool pool = new Pool(original);
+        _pools.Add(original.name, pool);
     }
 }
--- Assets/@Scripts/Managers/Core/ResourceManager.cs
+++ Assets/@Scripts/Managers/Core/ResourceManager.cs
@@ -1,18 +1,122 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using Object = UnityEngine.Object;
 
-public class ResourceManager : MonoBehaviour
+public class ResourceManager
 {
-    // Start is called before the first frame update
-    void Start()
+    private Dictionary<string, UnityEngine.Object> _resources = new Dictionary<string, Object>();
+    private Dictionary<string, AsyncOperationHandle> _handles = new Dictionary<string, AsyncOperationHandle>();
+
+    #region Load Resource
+    public T Load<T>(string key) where T : Object
     {
-        
+        if(_resources.TryGetValue(key, out Object resource))
+        {
+            return resource as T;
+        }
+
+        return null;
     }
 
-    // Update is called once per frame
-    void Update()
+    public GameObject Instantiate(string key, Transform parent = null, bool pooling = false)
     {
-        
+        GameObject prefab = Load<GameObject>(key);
+        if(null == prefab)
+        {
+            Debug.LogError($"Failed to load prefab : {key}");
+            return null;
+        }
+
+        if (pooling)
+        {
+            return Managers.Pool.Pop(prefab);
+        }
+
+        GameObject go = Object.Instantiate(prefab, parent);
+        go.name = prefab.name;
+
+        return go;
     }
+
+    public void Destroy(GameObject go)
+    {
+        if (null == go) { return; }
+        if (Managers.Pool.Push(go)) { return; }
+
+        Object.Destroy(go);
+    }
+
+    #endregion
+
+    #region Addressables
+    private void LoadAsync<T>(string key, Action<T> callback = null) where T : Object
+    {
+        // Cache
+        if(_resources.TryGetValue(key, out Object resource))
+        {
+            callback?.Invoke(resource as T);
+            return;
+        }
+
+        string loadKey = key;
+        if(key.Contains(".sprite"))
+        {
+            loadKey = $"{key}[{key.Replace(".sprite", "")}]";
+        }
+
+        var asyncOperation = Addressables.LoadAssetAsync<T>(loadKey);
+        asyncOperation.Completed += (op) => 
+        {
+            _resources.Add(key, op.Result);
+            _handles.Add(key, asyncOperation);
+            callback?.Invoke(op.Result);
+        };
+    }
+
+    public void LoadAllAsync<T>(string label, Action<string, int, int> callback) where T : UnityEngine.Object
+    {
+        var opHandle = Addressables.LoadResourceLocationsAsync(label, typeof(T));
+        opHandle.Completed += (op) =>
+        {
+            int loadCount = 0;
+            int totalCount = op.Result.Count;
+
+            foreach(var result in op.Result)
+            {
+                if(result.PrimaryKey.Contains(".sprite")) // sprite일 경우 일반적인 방법으로 로드하면 Texture2D로 인식하기 때문에 따로 처리
+                {
+                    LoadAsync<Sprite>(result.PrimaryKey, (obj) => 
+                    {
+                        loadCount++;
+                        callback?.Invoke(result.PrimaryKey, loadCount, totalCount);
+                    });
+                }
+                else
+                {
+                    LoadAsync<T>(result.PrimaryKey, (obj) => 
+                    {
+                        loadCount++;
+                        callback?.Invoke(result.PrimaryKey, loadCount, totalCount);
+                    });
+                }
+            }
+        };
+    }
+
+    public void Clear()
+    {
+        _resources.Clear();
+
+        foreach(var handle in _handles)
+        {
+            Addressables.Release(handle);
+        }
+
+        _handles.Clear();
+    }
+    #endregion
 }
